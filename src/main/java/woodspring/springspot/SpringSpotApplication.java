68fca981b51d68823504e52f31888b8489e782fd
woodspring <woodspring.toronto@gmail.com>
2020-08-24 22:13:32 -0400

    create Spring boot project

---
+++ src/main/java/woodspring/springspot/SpringSpotApplication.java
@@ -0,0 +1,13 @@
+package woodspring.springspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woodspring/springspot/SpringSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
