e34546a30bcf0dcf70a6f6985db7f4062c8981be
woodspring <woodspring.toronto@gmail.com>
2020-09-13 09:44:49 -0400

    for loop

--- src/main/java/woodspring/springspot/controller/SpotController.java
+++ src/main/java/woodspring/springspot/controller/SpotController.java
@@ -2,6 +2,7 @@ package woodspring.springspot.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import woodspring.springspot.model.FXSpot;
+import woodspring.springspot.service.SpotService;
 import woodspring.springspot.utils.FxSpotRate;
 
 @RestController
@@ -24,7 +28,7 @@ public class SpotController {
 	private final static Logger logger = LoggerFactory.getLogger(SpotController.class);
 	
 	@Autowired
-	FxSpotRate fxSpotRate;
+	SpotService spotService;
 	
 	@PostMapping(value="/new")
 	public ResponseEntity<FXSpot> newFXSpot(@RequestBody FXSpot fxSpot) {
@@ -38,17 +42,46 @@ public class SpotController {
 		
 	}
 	
-	@GetMapping("/Qoute")
+	@GetMapping("/Quotes")
 	@ResponseBody
-	public List<FXSpot> getQuote() {
-		List<FXSpot> retList = new ArrayList<>();
-		for (int ind=0; ind < 100; ind++) {
-			retList.add(fxSpotRate.getSymbol());
-		}
-		
+	public List<FXSpot> getFxQuote() {
+		String symbol = null;
+		List<FXSpot> retList = spotService.getQuote( symbol);
+		return retList;
+	}
+	
+	@GetMapping("/Quote/{symbol}")
+	public List<FXSpot> requestQoute(@PathVariable String symbol) {
+		List<FXSpot> retList = spotService.getQuote( symbol);
+		return retList;		
+	}
+	
+	@GetMapping("/Quote")
+	@ResponseBody
+	public List<FXSpot> requestForQuote(@RequestParam Map<String, String> requestParams) { 
+		logger.info("request:{}", requestParams);
+		String symbol = requestParams.get("symbol");
+		String tenor = requestParams.get("tenor");
+		logger.info("request:symbol:{}, tenor:{}", symbol, tenor);
+		List<FXSpot> retList = spotService.getQuote(symbol,  tenor);
 		return retList;
 		
 	}
 	
+	
+//	@GetMapping("/Qoute/{symbol}")
+//	public List<FXSpot> getQuote(@PathVariable String symbol) {
+//		List<FXSpot> retList = spotService.getQuote( symbol );
+//		return retList;	
+//	}
+//	
+//	
+//	@GetMapping("/Qoute")
+//	@ResponseBody
+//	public List<FXSpot> getQuoteST(@RequestParam String symbol, @RequestParam tenor) {
+//		List<FXSpot> retList = spotService.getQuote( "USDCAD");
+//		return retList;
+//		
+//	}
 
 }
--- src/main/java/woodspring/springspot/service/impl/SpotServiceImpl.java
+++ src/main/java/woodspring/springspot/service/impl/SpotServiceImpl.java
@@ -3,11 +3,15 @@ package woodspring.springspot.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import woodspring.springspot.model.FXSpot;
 import woodspring.springspot.service.SpotService;
 import woodspring.springspot.utils.FxSpotRate;
+import woodspring.springspot.model.SymbolTenor;
 
+
+@Service
 public class SpotServiceImpl implements SpotService {
 	
 	@Autowired
@@ -29,10 +33,22 @@ public class SpotServiceImpl implements SpotService {
 	@Override
 	public List<FXSpot> getQuote(String symbol) {
 		int num = (int)(Math.random()*50);
-		List<FXSpot> retList = fxRate.getFxQuote( symbol, num);
+		List<FXSpot> retList = null;
+		if ( symbol == null) {
+			retList = fxRate.getFxQuote(num);
+		}	else {
+			retList = fxRate.getFxQuote( symbol, num);
+		}
 		return retList;
 	}
 
+	@Override
+	public List<FXSpot> getQuote(String symbol, String tenor) {
+		int num = (int)(Math.random()*50);
+		List<FXSpot> retList = fxRate.queryQuote(symbol, tenor);
+		return retList;
+	}
+	
 	@Override
 	public FXSpot removeQoute(String symbol, String tenor) {
 		// TODO Auto-generated method stub
